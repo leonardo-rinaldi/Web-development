1d20274eedf7e51d2cc42e16a558534fbd5e5115
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-08-02 18:28:30 +0200

    Add files via upload

---
+++ Php/Exercise/Esercizio-variabili.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Dichiarare le seguenti variabili con i rispettivi tipi:
+ * 
+ * $boolean : Deve avere un valore booleano uguale a false
+ * $number : Deve avere un valore di tipo numerico
+ * $string: Deve avere un valore di tipo string uguale a: php7
+ * $arr : Deve essere un array e il settimo elemento ha il valore numerico 1234
+ *
+ */
+
+$boolean = false;
+$number = 12;
+$string = 'php7';
+$arr = array();
+$arr = array(0, 1, 2, 3, 4, 5, 6, 1234);
