aff7f94038c30529de4d14a1ab76fee0f4cdf5d5
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-10-28 11:10:08 +0100

    Add files via upload

---
+++ JavaScript/Learn/Esempio azioni su NodeList/js/add-content.js
@@ -0,0 +1,8 @@
+var hotItems = document.querySelectorAll("li.hot");
+
+if(hotItems.length > 0) {
+    
+    for(var i = 0; i < hotItems.length; i++) {
+        hotItems[i].className = "cool"; 
+    }
+}
