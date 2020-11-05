fe75c8a56b796b96e7cfbea5b6a12c99d0e5e1ad
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-11-05 18:36:06 +0100

    Add files via upload

---
+++ JavaScript/Esercizi/Esercizi funzioni/Esercizio-10/Esercizio-10.js
@@ -0,0 +1,58 @@
+/*
+  Pa-pa-pa lindomo
+  Scrivi una funzione che prenda in input una stringa e restituisca TRUE se è palindroma, FALSE se non lo è.
+  Nel controllo scarta gli spazi e i segni di punteggiatura.
+  Esempio:
+    Input: i topi non avevano nipoti
+    Output: TRUE
+  Consigli:
+  Puoi eliminare spazi e segni di punteggiatura usando le espressioni regolari o il metodo del tipo stringa chiamato replace,
+  in questo modo: str.replace(/\W/g, "").
+*/
+
+function controllaSePalindroma(stringa) {
+    var stringaRivoltata = giraStringa(stringa);
+    stringa = rimuoviSpazi(stringa);
+    var risultato;
+
+    if(stringa == stringaRivoltata) {
+        risultato = true;
+    } else {
+        risultato = false;
+    }
+    return risultato;
+}
+
+/*
+    Scrive la stringa da destra a sinistra e rimuove gli spazi
+*/
+
+function giraStringa(stringa) {
+    var stringaRivoltata = "";
+
+    for(var i = stringa.length; i >= 0; i--) {
+            stringaRivoltata += stringa.charAt(i);
+    }
+    stringaRivoltata = rimuoviSpazi(stringaRivoltata);
+    return stringaRivoltata;
+}
+
+/*
+    Rimuovo gli spazi dalla stringa
+*/
+
+function rimuoviSpazi(stringa) {
+    var stringaSenzaSpazi = "";
+    
+    for(var i = 0; i < stringa.length; i++) {
+        if(stringa.charAt(i) == " ") {
+            continue;
+        } else {
+            stringaSenzaSpazi += stringa.charAt(i);
+        }
+    }
+    return stringaSenzaSpazi;
+}
+
+
+document.write(controllaSePalindroma("i topi non avevano nipoti"))
