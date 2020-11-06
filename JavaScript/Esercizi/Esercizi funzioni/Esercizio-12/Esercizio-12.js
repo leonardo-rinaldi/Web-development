9e60df252210061e4456ab97de42966f5f8b584e
Leonardo Rinaldi <rinaldileonardo99@gmail.com>
2020-11-06 20:56:48 +0100

    Add files via upload

---
+++ JavaScript/Esercizi/Esercizi funzioni/Esercizio-12/Esercizio-12.js
@@ -0,0 +1,58 @@
+/*
+  I primi numeri…primi!
+  Scrivi una funzione che prenda in input un numero e restituisca TRUE se è un numero primo, FALSE altrimenti.
+  Scrivi una seconda funzione, che prenda in input un numero N e stampi i primi N numeri primi.
+  Esempio:
+    Input: n = 5
+    Output:
+          TRUE
+            2
+            3
+            5
+            7
+            11
+*/
+
+/*
+    Controlla se il numero n inserito è primo.
+*/
+
+function controllaSePrimo(n) {
+    var risultato = true;
+
+    if(n == 1 || n == 2) {
+        risultato = true;
+    } else {
+        for(var i = 2; i < n; i++) {
+            if(n % i == 0) {
+                risultato = false;
+            }
+        }
+    }
+    return risultato;
+}
+
+/*
+    Stampa i primi n numeri primi, se il numero è primo l'indice i viene
+    aumentato di 1 e si passa al valore successivo da controllare aumentando
+    il valore di 1.
+*/
+
+function stampaNumeriPrimi(n) {
+    var i = 0;          //indice
+    var valore = 2;     //valore iniziale
+    while(i < n) {
+        if(controllaSePrimo(valore)) {
+            document.write("<br>");
+            document.write(valore);
+            i++;
+            valore++;
+        } else {
+            valore++;
+        }
+    }
+}
+
+document.write(controllaSePrimo(30));
+document.write("<br>");
+stampaNumeriPrimi(5);
